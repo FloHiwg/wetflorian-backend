b7b96eecabf692a3cbe6717e93ac376400d645ab
Florian Heiwig <florian.hiwg@gmail.com>
2019-05-12 19:29:53 +0200

    Added jackson strategy to fetch data

--- src/main/kotlin/de/nist/wetflorianbackend/wetflorianbackend/entity/PlantStatus.kt
+++ src/main/kotlin/de/nist/wetflorianbackend/wetflorianbackend/entity/PlantStatus.kt
@@ -1,5 +1,9 @@
 package de.nist.wetflorianbackend.wetflorianbackend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonManagedReference
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
 import java.util.*
 import javax.persistence.*
 
@@ -8,7 +12,10 @@ data class PlantStatus (
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long,
-    @ManyToOne val plant: Plant?,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "plantId", nullable = false)
+    @JsonManagedReference
+    val plant: Plant?,
     val timestamp: Date,
     val value: String
 )
